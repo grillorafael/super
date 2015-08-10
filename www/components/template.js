1ce68b0d1d96b6c5fc3ef4a62d3fbd9d318eb541
Rafael Grillo <grillorafael@gmail.com>
2015-08-10 13:44:10 -0300

    zzzzzzz

--- www/components/template.js
+++ www/components/template.js
@@ -1,6 +1,7 @@
 angular.module('super-template', []).run(['$templateCache', function($templateCache) {
   $templateCache.put('components/nav-header/nav-header.html', '<header class="super-nav-header">\n    <ng-transclude></ng-transclude>\n</header>');
-  $templateCache.put('components/side-menu-item/side-menu-item.html', '<div class="side-menu-item">\n    <h3>{{small}}</h3>\n    <ng-transclude></ng-transclude>\n</div>');
+  $templateCache.put('components/side-menu-item/side-menu-item.html', '<div class="side-menu-item">\n    <p>{{small}}</p>\n    <ng-transclude></ng-transclude>\n</div>');
   $templateCache.put('components/side-menu/side-menu.html', '<nav class="super-side-menu">\n    <ng-transclude></ng-transclude>\n</nav>');
   $templateCache.put('components/super-footer/super-footer.html', '<footer class="super-footer">\n    <ng-transclude></ng-transclude>\n</footer>');
+  $templateCache.put('components/super-frame/super-frame.html', '<section class="super-frame">\n    <header>\n        {{title}}\n    </header>\n    <section>\n        <ng-transclude></ng-transclude>\n    </section>\n</section>');
 }]);
