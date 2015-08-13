59ba2b8f9f35fd5ba865b0538882e5058bca7107
Rafael Grillo <grillorafael@gmail.com>
2015-08-13 12:18:09 -0300

    experimenting a bit more

--- www/components/side-menu-item/side-menu-item.directive.js
+++ www/components/side-menu-item/side-menu-item.directive.js
@@ -12,7 +12,14 @@
             },
             templateUrl: 'components/side-menu-item/side-menu-item.html',
             link: function (scope, element, attrs) {
-
+                // if(element[0].getElementsByClassName('side-menu-item').length > 0) {
+                //     element.addClass('has-children');
+                //     element.on('click', function (e) {
+                //         element.toggleClass('expanded');
+                //         e.preventDefault();
+                //         return false;
+                //     });
+                // }
             }
         };
     }
