8b0a5a143e7809908322bad5ab6929baaf6ba4b0
Rafael Grillo <grillorafael@gmail.com>
2015-08-13 23:56:43 -0300

    enhances sidemenu component

--- www/components/side-menu-item/side-menu-item.directive.js
+++ www/components/side-menu-item/side-menu-item.directive.js
@@ -12,14 +12,34 @@
             },
             templateUrl: 'components/side-menu-item/side-menu-item.html',
             link: function (scope, element, attrs) {
-                // if(element[0].getElementsByClassName('side-menu-item').length > 0) {
-                //     element.addClass('has-children');
-                //     element.on('click', function (e) {
-                //         element.toggleClass('expanded');
-                //         e.preventDefault();
-                //         return false;
-                //     });
-                // }
+                function numberOfChildren() {
+                    return element[0].getElementsByClassName('side-menu-item').length;
+                }
+
+                function setSubmenuHeight(height) {
+                    element[0].style.marginBottom = height + 'px';
+                    element[0].getElementsByTagName('ul')[0].style.height = height + 'px';
+                }
+
+                var isCollapsed = true;
+                if(element[0].getElementsByClassName('side-menu-item').length > 0) {
+                    element.addClass('has-submenu');
+
+                    angular.element(element[0].children[1]).on('click', function (e) {
+                        isCollapsed = !isCollapsed;
+                        element.toggleClass('collapsed');
+                        if(isCollapsed) {
+                            setSubmenuHeight(0);
+                        }
+                        else {
+                            setSubmenuHeight(numberOfChildren() * 40 + 8);
+                        }
+
+                        // Preventing route TODO Remove this behaviour
+                        e.preventDefault();
+                        return false;
+                    });
+                }
             }
         };
     }
--- www/components/template.js
+++ www/components/template.js
@@ -1,6 +1,6 @@
 angular.module('super-template', []).run(['$templateCache', function($templateCache) {
   $templateCache.put('components/nav-header/nav-header.html', '<header class="super-nav-header">\n    <ng-transclude></ng-transclude>\n</header>');
-  $templateCache.put('components/side-menu-item/side-menu-item.html', '<div class="side-menu-item" small="{{small}}">\n    <ng-transclude></ng-transclude>\n</div>');
-  $templateCache.put('components/side-menu/side-menu.html', '<nav class="super-side-menu">\n    <ng-transclude></ng-transclude>\n</nav>');
+  $templateCache.put('components/side-menu-item/side-menu-item.html', '<li class="side-menu-item" small="{{small}}">\n    <ng-transclude></ng-transclude>\n    <i class="fa fa-chevron-left collapse-icon"></i>\n</li>');
+  $templateCache.put('components/side-menu/side-menu.html', '<nav class="super-side-menu">\n    <ul ng-transclude></ul>\n</nav>');
   $templateCache.put('components/super-footer/super-footer.html', '<footer class="super-footer">\n    <ng-transclude></ng-transclude>\n</footer>');
 }]);
